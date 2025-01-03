66a72589b7c8c6dfb58552820622f8fa55bff61e
oussama2505 <oussama.brahmi0608@gmail.com>
2025-01-03 12:02:14 +0100

    install nelify-angular package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule,
+        CommonModule
     ],
 })
 export class AppModule{}
